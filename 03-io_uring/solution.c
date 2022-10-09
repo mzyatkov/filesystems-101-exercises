8380069f3b8671775d64d394b7fe8dcaf0c709ef
Dominique Lefevre <root@/dev/null>
2022-10-09 07:49:21 -0400

    03-io_uring: add a not-a-solution.

--- 03-io_uring/solution.c
+++ 03-io_uring/solution.c
@@ -1,10 +1,45 @@
 #include <solution.h>
 
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fs_malloc.h>
+#include <errno.h>
+
 int copy(int in, int out)
 {
-	(void) in;
-	(void) out;
+	struct stat st_in, st_out;
+	int r;
 
-	/* implement me */
-	return 0;
+	if ((r = fstat(in, &st_in)) ||
+	    (r = fstat(out, &st_out)))
+	{
+		return -errno;
+	}
+
+	size_t len = st_in.st_size;
+	void *buf = fs_xmalloc(len);
+	r = pread(in, buf, len, 0);
+	if (r < 0) {
+		r = -errno;
+		goto out;
+	}
+	if ((size_t)r != len) {
+		r = -EAGAIN;
+		goto out;
+	}
+
+	r = write(out, buf, len);
+	if (r < 0) {
+		r = -errno;
+		goto out;
+	}
+	if ((size_t)r != len) {
+		r = -EAGAIN;
+		goto out;
+	}
+
+	r = 0;
+out:
+	fs_xfree(buf);
+	return r;
 }
