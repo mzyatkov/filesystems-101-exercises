0bbac0e130a93c52872c5ebd13b6aa727a97d373
mzyatkov <ziatkov.mv@phystech.edu>
2022-10-19 23:40:00 +0300

    02 fix erofs

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -35,7 +35,8 @@ static int read_hellofs(const char *path, char *buf, size_t size, off_t offset, 
 
 	return -ENOENT;
 }
-static int write_hellofs(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+static int write_hellofs(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
 	(void)path;
 	(void)buf;
 	(void)size;
@@ -48,7 +49,9 @@ static int open_hellofs(const char *path, struct fuse_file_info *fi)
 {
 	(void)fi;
 	if (strcmp(path, HELLO_PATH) != 0)
-		{return -ENOENT;}
+	{
+		return -EROFS;
+	}
 	return 0;
 }
 static int getattr_hellofs(const char *path, struct stat *stbuf, struct fuse_file_info *fi)
@@ -89,9 +92,104 @@ static int readdir_hellofs(const char *path, void *buf, fuse_fill_dir_t filler, 
 	}
 	filler(buf, ".", NULL, 0, 0);
 	filler(buf, "..", NULL, 0, 0);
-	filler(buf, HELLO_PATH+1, NULL, 0, 0);
+	filler(buf, HELLO_PATH + 1, NULL, 0, 0);
 
 	return 0;
+}
+
+static int access_hellofs(const char *path, int mode) {
+	if (mode & W_OK)
+        return -EROFS;
+	return access(path, mode);
+}
+static int setxattr_hellofs(const char *path, const char *name, const char *value, size_t size, int flags)
+{
+	(void)path;
+	(void)name;
+	(void)value;
+	(void)size;
+	(void)flags;
+	return -EROFS;
+}
+static int removexattr_hellofs
+(const char *path, const char *name)
+{
+	(void)path;
+  	(void)name;
+  	return -EROFS;
+}
+static int mknod_hellofs(const char *path, mode_t mode, dev_t rdev)
+{
+  (void)path;
+  (void)mode;
+  (void)rdev;
+  return -EROFS;
+}
+static int mkdir_hellofs(const char *path, mode_t mode)
+{
+  (void)path;
+  (void)mode;
+  return -EROFS;
+}
+
+static int unlink_hellofs(const char *path)
+{
+  (void)path;
+  return -EROFS;
+}
+
+static int rmdir_hellofs(const char *path)
+{
+  (void)path;
+  return -EROFS;
+}
+
+static int symlink_hellofs(const char *from, const char *to)
+{
+  (void)from;
+  (void)to;
+  return -EROFS;	
+}
+
+static int rename_hellofs(const char *from, const char *to, unsigned int flags)
+{
+	(void)flags;
+  (void)from;
+  (void)to;
+  return -EROFS;
+}
+
+static int link_hellofs(const char *from, const char *to)
+{
+  (void)from;
+  (void)to;
+  return -EROFS;
+}
+
+static int chmod_hellofs(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+	(void)fi;
+  (void)path;
+  (void)mode;
+  return -EROFS;
+    
+}
+
+static int chown_hellofs(const char *path, uid_t uid, gid_t gid,  struct fuse_file_info *fi)
+{
+	(void)fi;
+  (void)path;
+  (void)uid;
+  (void)gid;
+  return -EROFS;
+}
+
+static int truncate_hellofs(const char *path, off_t size, struct fuse_file_info *fi)
+{
+	(void)fi;
+	(void)path;
+  	(void)size;
+  	return -EROFS;
 }
 
 static const struct fuse_operations hellofs_ops = {
@@ -99,8 +197,20 @@ static const struct fuse_operations hellofs_ops = {
 	.readdir = readdir_hellofs,
 	.open = open_hellofs,
 	.read = read_hellofs,
-	.write = write_hellofs
-
+	.write = write_hellofs,
+	.access = access_hellofs,
+	.chmod = chmod_hellofs,
+	.chown = chown_hellofs,
+	.setxattr = setxattr_hellofs,
+	.truncate = truncate_hellofs,
+	.mknod = mknod_hellofs,
+	.mkdir = mkdir_hellofs,
+	.removexattr = removexattr_hellofs,
+	.rename = rename_hellofs,
+	.symlink = symlink_hellofs,
+	.unlink = unlink_hellofs,
+	.link = link_hellofs,
+	.rmdir = rmdir_hellofs
 };
 
 int helloworld(const char *mntp)
