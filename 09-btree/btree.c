4a0eb4c9d4460b39005de72038907fcb3d277d4b
Dominique Lefevre <root@/dev/null>
2022-10-25 05:51:48 -0400

    09-btree: add not-a-solution.

--- 09-btree/btree.c
+++ 09-btree/btree.c
@@ -1,63 +1,63 @@
 #include <solution.h>
-#include <stdlib.h>
+#include <bmap.h>
+#include <fs_malloc.h>
+
+#define MAX_KEY (64 * 1024 * 1024)
 
 struct btree
 {
-	/* implement me */
+	struct bmap *bmap;
 };
 
 struct btree* btree_alloc(unsigned int L)
 {
 	(void) L;
 
-	return NULL;
+	struct btree *t = fs_xmalloc(sizeof(*t));
+	t->bmap = bmap_alloc(MAX_KEY);
+	return t;
 }
 
 void btree_free(struct btree *t)
 {
-	(void) t;
+	bmap_free(t->bmap);
+	fs_xfree(t);
 }
 
 void btree_insert(struct btree *t, int x)
 {
-	(void) t;
-	(void) x;
+	bmap_insert(t->bmap, x);
 }
 
 void btree_delete(struct btree *t, int x)
 {
-	(void) t;
-	(void) x;
+	bmap_delete(t->bmap, x);
 }
 
 bool btree_contains(struct btree *t, int x)
 {
-	(void) t;
-	(void) x;
-
-	return false;
+	return bmap_contains(t->bmap, x);
 }
 
 struct btree_iter
 {
+	struct bmap_iter *i;
 };
 
 struct btree_iter* btree_iter_start(struct btree *t)
 {
-	(void) t;
-
-	return NULL;
+	struct btree_iter *i = fs_xmalloc(sizeof(*i));
+	i->i = bmap_iter_start(t->bmap);
+	return i;
 }
 
 void btree_iter_end(struct btree_iter *i)
 {
-	(void) i;
+	bmap_iter_end(i->i);
+	fs_xfree(i);
 }
 
 bool btree_iter_next(struct btree_iter *i, int *x)
 {
-	(void) i;
-	(void) x;
-
-	return false;
+	return bmap_iter_next(i->i, x);
 }
