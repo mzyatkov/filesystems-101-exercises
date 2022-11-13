fc0051cf5d3bb27db5f704971df68d5844ff36f4
mzyatkov <ziatkov.mv@phystech.edu>
2022-11-13 19:08:38 +0300

    07 add ENOTDIR return to libntfs source code......

--- 07-ntfs-read-file/solution.c
+++ 07-ntfs-read-file/solution.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 #define _STRUCT_TIMESPEC 1
 #include <stdlib.h>
 
@@ -12,6 +13,142 @@
 #include <ntfs-3g/attrib.h>
 #include <ntfs-3g/volume.h>
 #include <ntfs-3g/dir.h>
+
+// rewrite libntfs-3g function
+ntfs_inode *ntfs_pathname_to_inode(ntfs_volume *vol, ntfs_inode *parent,
+								   const char *pathname)
+{
+	u64 inum;
+	int len, err = 0;
+	char *p, *q;
+	ntfs_inode *ni;
+	ntfs_inode *result = NULL;
+	ntfschar *unicode = NULL;
+	char *ascii = NULL;
+
+	if (!vol || !pathname)
+	{
+		errno = EINVAL;
+		return NULL;
+	}
+
+	ntfs_log_trace("path: '%s'\n", pathname);
+
+	ascii = strdup(pathname);
+	if (!ascii)
+	{
+		ntfs_log_error("Out of memory.\n");
+		err = ENOMEM;
+		goto out;
+	}
+
+	p = ascii;
+	/* Remove leading /'s. */
+	while (p && *p && *p == PATH_SEP)
+		p++;
+
+	if (parent)
+	{
+		ni = parent;
+	}
+	else
+	{
+
+		ni = ntfs_inode_open(vol, FILE_root);
+		if (!ni)
+		{
+			ntfs_log_debug("Couldn't open the inode of the root "
+						   "directory.\n");
+			err = EIO;
+			result = (ntfs_inode *)NULL;
+			goto out;
+		}
+	}
+
+	while (p && *p)
+	{
+		/* Find the end of the first token. */
+		q = strchr(p, PATH_SEP);
+		if (q != NULL)
+		{
+			*q = '\0';
+		}
+
+		len = ntfs_mbstoucs(p, &unicode);
+		if (len < 0)
+		{
+			ntfs_log_perror("Could not convert filename to Unicode:"
+							" '%s'",
+							p);
+			err = errno;
+			goto close;
+		}
+		else if (len > NTFS_MAX_NAME_LEN)
+		{
+			err = ENAMETOOLONG;
+			goto close;
+		}
+		inum = ntfs_inode_lookup_by_name(ni, unicode, len);
+
+		if (inum == (u64)-1)
+		{
+			ntfs_log_debug("Couldn't find name '%s' in pathname "
+						   "'%s'.\n",
+						   p, pathname);
+			err = ENOENT;
+			goto close;
+		}
+
+		if (ni != parent)
+			if (ntfs_inode_close(ni))
+			{
+				err = errno;
+				goto out;
+			}
+
+		inum = MREF(inum);
+		ni = ntfs_inode_open(vol, inum);
+		if (!ni)
+		{
+			ntfs_log_debug("Cannot open inode %llu: %s.\n",
+						   (unsigned long long)inum, p);
+			err = EIO;
+			goto close;
+		}
+
+		// if ni is directory
+		if (q)
+		{
+			if (!(ni->mrec->flags & MFT_RECORD_IS_DIRECTORY))
+			{
+				err = ENOTDIR;
+				goto close;
+			}
+		}
+
+		free(unicode);
+		unicode = NULL;
+
+		if (q)
+			*q++ = PATH_SEP; /* JPA */
+		p = q;
+		while (p && *p && *p == PATH_SEP)
+			p++;
+	}
+
+	result = ni;
+	ni = NULL;
+close:
+	if (ni && (ni != parent))
+		if (ntfs_inode_close(ni) && !err)
+			err = errno;
+out:
+	free(ascii);
+	free(unicode);
+	if (err)
+		errno = err;
+	return result;
+}
 
 int dump_file(int img, const char *path, int out)
 {
@@ -37,6 +174,7 @@ int dump_file(int img, const char *path, int out)
 
 	if (!inode)
 	{
+		printf("%d\n", errno);
 		int err = -errno;
 		ntfs_umount(volume, FALSE);
 		return err;
