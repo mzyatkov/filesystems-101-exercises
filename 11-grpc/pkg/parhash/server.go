aeaa1219466db472bdfc957ed40888668477b361
mzyatkov <ziatkov.mv@phystech.edu>
2022-12-04 21:34:08 +0300

    11 alpha version

--- 11-grpc/pkg/parhash/server.go
+++ 11-grpc/pkg/parhash/server.go
@@ -5,7 +5,14 @@ import (
 
 	"github.com/pkg/errors"
 	"golang.org/x/sync/semaphore"
+
+	hashpb "fs101ex/pkg/gen/hashsvc"
+	"google.golang.org/grpc"
+	"crypto/sha256"
+	"net"
+	"sync"
 )
+
 
 type Config struct {
 	ListenAddr   string
@@ -39,6 +46,11 @@ type Server struct {
 	conf Config
 
 	sem *semaphore.Weighted
+
+	stop context.CancelFunc
+	l    net.Listener
+	wg   sync.WaitGroup
+
 }
 
 func New(conf Config) *Server {
@@ -48,19 +60,46 @@ func New(conf Config) *Server {
 	}
 }
 
-func (s *Server) Start(ctx context.Context) (err error) {
-	defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+func (s *Server) Start(ctx context.Context) (err error) {
+	defer func() { err = errors.Wrapf(err, "Start()") }()
+
+	ctx, s.stop = context.WithCancel(ctx)
+
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	srv := grpc.NewServer()
+	hashpb.RegisterHashSvcServer(srv, s)
+
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+
+		srv.Serve(s.l)
+	}()
+	go func() {
+		defer s.wg.Done()
+
+		<-ctx.Done()
+		s.l.Close()
+	}()
 
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+}
+
+func (s *Server) Hash(ctx context.Context, req *hashpb.HashReq) (resp *hashpb.HashResp, err error) {
+	h := sha256.Sum256(req.Data)
+	return &hashpb.HashResp{Hash: h[:]}, nil
 }
